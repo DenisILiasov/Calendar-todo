d07c2b3fa62dd6d54ad23dc94a6324b671acd0b8
Iliasov Denis <ilasovdenis01@gmail.com>
2023-09-12 20:54:27 +0500

    safd

--- src/components/calendare/days/day/titleDay/titleDay.jsx
+++ src/components/calendare/days/day/titleDay/titleDay.jsx
@@ -4,14 +4,6 @@ import ModalWindow from '../../../../ui/modal/modal'
 
 const TitleDay = (props) => {
 
-    const [modal , setModal] = useState(false)
-    const openModal = () => {
-        setModal(true)
-    }
-    const clouseModal = () => {
-        setModal(false)
-    }
-
     if(props.status !== undefined){
         return(
             <div className={style.headerContainer}>
@@ -42,37 +34,27 @@ const TitleDay = (props) => {
                         <div>{props.number}</div>
                     </div>
                     <div className={style.border}>
-                        <button onClick = {openModal} className={style.addTask}>
+                        <button onClick = {props.open} className={style.addTask}>
                             +
                         </button>
                     </div>
                 </div>
-                <ModalWindow clouse={clouseModal} openModal={modal}>
-                    <div className={style.addTask}>
-                        Чингиз ну ты еблан зачем нажал?
-                    </div>
+                <ModalWindow>
+
                 </ModalWindow>
             </>
         )
     }else{
         return(
-            <>
-              <div className={style.headerContainer}>
-                    <div className={props.styles.join(' ')}>
-                        <div className={style.number}>{props.number}</div>
-                        <div>{days[props.day]}</div>
-                    </div>
-                    <button onClick = {openModal} className={style.addTask}>
-                        +
-                    </button>
+            <div className={style.headerContainer}>
+                <div className={props.styles.join(' ')}>
+                    <div className={style.number}>{props.number}</div>
+                    <div>{days[props.day]}</div>
                 </div>
-                <ModalWindow clouse={clouseModal} openModal={modal}>
-                    <div className={style.addTask}>
-                        Чингиз ну ты еблан зачем нажал?
-                    </div>
-                </ModalWindow>
-            </>
-          
+                <button onClick = {props.open} className={style.addTask}>
+                    +
+                </button>
+            </div>
         )
     }
     
